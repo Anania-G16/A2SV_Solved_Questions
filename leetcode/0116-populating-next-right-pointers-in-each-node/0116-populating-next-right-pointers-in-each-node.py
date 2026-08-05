7b8c8544b2ad6217298a477ed5d433ab21de07f8
Anania Girma <ananiagirma42@gmail.com>
2026-08-05 11:02:04 +0300

    Add solution for Populating Next Right Pointers in Each Node

---
+++ leetcode/0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
@@ -0,0 +1,29 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if not root:
+            return None
+        queue = deque([root])
+
+        while queue:
+            size = len(queue)
+            for i in range(size):
+                temp = queue.popleft()
+                if i == size-1:
+                    temp.next = None
+                else:
+                    temp.next = queue[0]
+                if temp.left:
+                    queue.append(temp.left)
+                if temp.right:
+                    queue.append(temp.right)
+        return root
